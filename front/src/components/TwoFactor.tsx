129a9743630b9b8e841842c6a4ae04efd7e8c630
slo <selbihafid@gmail.com>
2023-11-28 19:22:27 +0100

    nothing

--- front/src/components/TwoFactor.tsx
+++ front/src/components/TwoFactor.tsx
@@ -13,7 +13,42 @@ import useMediaQuery from "@mui/material/useMediaQuery";
 
 type User = {
   id_user: number;
-  name: string;
+  name: string;{fadeIn("down", 0.2)}
+  initial="hidden"
+  whileInView={"show"}
+  viewport={{ once: false, amount: 0.7 }}
+className="flex justify-center w-full text-white items-center text-6xl">
+{" "}
+<div>
+<canvas
+ref={myCanvas}
+className="bg-pale-blue absolute m-auto inset-0 rounded-main max-w-full max-h-full w-1088 h-644"
+id="canvas"
+></canvas>
+<p
+className="relative text-bluish-purple inset-0 text-2xl font-roboto font-light px-4 text-center"
+id="message"
+></p>
+<button
+className="btn text-bluish-purple bg-reddish-orange block relative text-4xl border-none rounded-main font-zenkaku text-center font-medium tracking-normal w-full my-2 mx-auto pt-1 px-7 pb-2 cursor-pointer"
+id="online-game"
+onClick={handleMultiplayerGameClick}
+>
+Play Online
+</button>
+<button
+className="btn text-pale-blue bg-bluish-purple block relative text-4xl border-none rounded-main font-zenkaku text-center font-medium tracking-normal w-full my-2 mx-auto pt-1 px-7 pb-2 cursor-pointer"
+id="bot-game"
+onClick={handleBotGameClick}
+>
+Play vs Bot
+</button>
+{/* <button
+className="hidden text-white bg-exit-red relative text-4xl border-none rounded-main font-zenkaku text-center font-medium tracking-normal my-2 mx-auto pt-1 px-7 pb-2 cursor-pointer"
+id="exit-btn"
+>
+Back Home
+</
   avatar: string;
   TwoFactor: boolean;
   secretKey: string | null;
