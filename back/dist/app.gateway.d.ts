b05530d28152c8ba9d28bca8c84dc5b0e9f75c5b
slo <selbihafid@gmail.com>
2023-11-29 22:07:03 +0100

    hafid: new updates

---
+++ back/dist/app.gateway.d.ts
@@ -0,0 +1,34 @@
+import { OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect } from "@nestjs/websockets";
+import { Server, Socket } from "socket.io";
+import { Data, Room, RoomBall, RoomPlayer } from "./interfaces";
+import { JwtService } from "./auth/jwt/jwtservice.service";
+import { PrismaService } from "./prisma.service";
+export declare class AppGateway implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
+    private jwt;
+    private prisma;
+    constructor(jwt: JwtService, prisma: PrismaService);
+    server: Server;
+    private roomsId;
+    private users;
+    private rooms;
+    private frRooms;
+    private framePerSec;
+    private isPaused;
+    private player01;
+    private player02;
+    private logger;
+    decodeCookie(client: Socket): any;
+    afterInit(server: Server): void;
+    handleConnection(client: Socket, ...args: any[]): Promise<void>;
+    handleDisconnect(client: Socket): Promise<void>;
+    handleJoinFriendsRoom(client: Socket, data: any): Promise<void>;
+    handleJoinRoom(client: Socket): Promise<void>;
+    handleUpdatePlayer(client: Socket, data: Data): void;
+    handleLeave(client: Socket, roomID: string): Promise<void>;
+    findRoomBySocketId(socketId: string): Room;
+    pauseGame(duration: number): void;
+    resetBall(room: Room): void;
+    updateScore(room: Room): void;
+    collision(ball: RoomBall, player: RoomPlayer): boolean;
+    startRoomGame(room: Room): void;
+}
